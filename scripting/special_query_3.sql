9a184302850242d004e663033e84670db3140f8a
Karam Masad <karammasad@tamu.edu>
2025-10-01 12:56:16 -0500

    Fixed special query 3 to return a table for each date and its top 10 order sums. Before it required inputting a specific date to the query.

--- scripting/special_query_3.sql
+++ scripting/special_query_3.sql
@@ -1,9 +1,20 @@
-SELECT SUM(COST) AS top_10_sum
-FROM (  SELECT placed_at, cost FROM orders
-        WHERE placed_at::date = 'YYYY-MM-DD'
+SELECT 
+        order_date,
+        SUM(COST) AS top_10_sum
+FROM (  
+        SELECT 
+        placed_at::date AS order_date, 
+        cost, 
+        ROW_NUMBER() OVER (
+        PARTITION BY placed_at::date
         ORDER BY cost DESC
-        LIMIT 10
-) AS subquery;
+        ) AS num
+        FROM orders
+) AS subquery
+WHERE num <= 10
+GROUP BY order_date
+ORDER BY top_10_sum DESC;
+
 
 
 
