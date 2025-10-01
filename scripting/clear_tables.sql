ee7ca31914e42c5fd44a103e9aaccb37cc256ee2
Robin <robinjefferson123@gmail.com>
2025-09-30 22:51:09 -0500

    add migration sql files

---
+++ scripting/clear_tables.sql
@@ -0,0 +1,8 @@
+-- The order here is important!
+DELETE FROM drinks_ingredients;
+DELETE FROM drinks_orders;
+DELETE FROM orders;
+DELETE FROM employees;
+DELETE FROM ingredients;
+DELETE FROM menu;
+DELETE FROM categories;
---
+++ scripting/delete_tables.sql
@@ -0,0 +1,8 @@
+-- The order here is important!
+DROP TABLE drinks_ingredients;
+DROP TABLE drinks_orders;
+DROP TABLE orders;
+DROP TABLE employees;
+DROP TABLE ingredients;
+DROP TABLE menu;
+DROP TABLE categories;
