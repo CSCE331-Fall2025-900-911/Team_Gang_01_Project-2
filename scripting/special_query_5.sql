ff468880e997359f4018cb2affd56c369352d56e
Robin <robinjefferson123@gmail.com>
2025-10-01 00:40:25 -0500

    Finish special query 5

--- scripting/special_query_5.sql
+++ scripting/special_query_5.sql
@@ -1,4 +1,36 @@
-SELECT id, SUM(cost) AS day_profits FROM orders 
-WHERE DATE_TRUNC('week', placed_at) = DATE_TRUNC('week', '<WEEK>'::DATE)
-GROUP BY CAST(placed_at AS DATE)
-ORDER BY day_profits;
+WITH week_orders AS (
+    select 
+        o.id,
+        CAST(o.placed_at as DATE) as day,
+        o.cost
+    from orders o
+    WHERE date_trunc('WEEK', o.placed_at) = date_trunc('WEEK', '2025-09-22'::DATE)
+),
+day_sales AS (
+    SELECT wo.day, SUM(wo.cost) AS revenue
+    FROM week_orders wo
+    GROUP BY wo.day
+),
+lowest_day AS (
+    SELECT ds.day, ds.revenue
+    FROM day_sales ds
+    ORDER BY ds.revenue ASC, day ASC
+    LIMIT 1
+),
+top_seller AS (
+    SELECT m.name AS drink, COUNT(*) AS num_sold
+    FROM lowest_day ld
+    JOIN week_orders   wo  ON wo.day = ld.day
+    JOIN drinks_orders dro ON dro.order_id = wo.id
+    JOIN menu          m   ON m.id = dro.menu_id
+    GROUP BY m.name
+    ORDER BY num_sold DESC, m.name
+    LIMIT 1
+)
+SELECT 
+    ld.day AS lowest_sales_day,
+    ld.revenue AS revenue,
+    ts.drink AS top_seller,
+    ts.num_sold
+FROM lowest_day ld
+JOIN top_seller ts ON TRUE;
